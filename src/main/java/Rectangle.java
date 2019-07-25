f0ab3aa505ef2e38aeb2c7aba14a46f55e7ecc82
kasambuli <cynthiakasambuli@gmail.com>
2019-07-25 12:13:25 +0300

    Created the main Rectangle class with the getter methods

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,21 @@
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+    public int getWidth() {
+        return this.width;
+    }
+    public boolean isSquare() {
+        return length == width;
+    }
+}
+
+
